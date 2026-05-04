1e90e509f66822ddf9113c5800b8ba271d3125b7
Baber19 <Baber19i@outlook.com>
2026-05-04 22:33:32 +0500

    Ecommerce App

--- lib/ui/on_boarding/pages/loginpage/login_page.dart
+++ lib/ui/on_boarding/pages/loginpage/login_page.dart
@@ -50,7 +50,7 @@ class LoginPage extends StatelessWidget {
                   }
                 },
                 decoration: InputDecoration(
-                  labelText: "E-mail",
+                  labelText: "Email",
                   hintText: "Enter your e-mail here..",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(21),
