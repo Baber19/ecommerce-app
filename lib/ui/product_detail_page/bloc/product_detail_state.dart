065eaf1801fe4c371f78ddf680def1a94697d5ed
Baber19 <Baber19i@outlook.com>
2026-05-19 23:26:40 +0500

    Ecommerce App

--- lib/ui/product_detail_page/bloc/product_detail_state.dart
+++ lib/ui/product_detail_page/bloc/product_detail_state.dart
@@ -2,3 +2,5 @@ abstract class ProductDetailState{}
 
 class ProductsInitialState extends ProductDetailState{}
 class ProductLoadingState extends ProductDetailState{}
+class ProductSuccessState extends ProductDetailState{}
+
