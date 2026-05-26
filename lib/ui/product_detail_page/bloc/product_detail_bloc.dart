6e807127cdab256351942d99d8e75b355724443f
Baber19 <Baber19i@outlook.com>
2026-05-26 22:41:17 +0500

    Ecommerce App

--- lib/ui/product_detail_page/bloc/product_detail_bloc.dart
+++ lib/ui/product_detail_page/bloc/product_detail_bloc.dart
@@ -1,1 +1,7 @@
-class ProductDetailBloc {}
+import 'package:ecommercer_app/ui/product_detail_page/bloc/product_detail_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProductDetailBloc extends Bloc<ProductDetailEvent,ProductDetailBloc>{
+  ProductDetailBloc(super.initialState);
+
+}
